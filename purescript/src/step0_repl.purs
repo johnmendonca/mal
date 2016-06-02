dab414b2c7f15efe401f9be7dfa46555d7e06aa0
John Mendonca <joaosinho@gmail.com>
2016-06-01 23:11:19 -0600

    purescript: use node-readline instead of simple-repl
    I thought maybe something about simple-repl might be causing the
    test integration issues, but this impl has the same problem: it is
    necessary to log the result twice as well as use the --no-pty on
    the make command for tests to work. Maybe due to the async nature
    of node readline.

--- purescript/src/step0_repl.purs
+++ purescript/src/step0_repl.purs
@@ -2,19 +2,23 @@ module Step0Repl where
 
 import Prelude
 import Control.Monad.Eff (Eff) 
-import Control.Monad.Eff.Console (CONSOLE)
+import Control.Monad.Eff.Console (CONSOLE, log)
+import Control.Monad.Eff.Exception (EXCEPTION)
 
-import Node.ReadLine (READLINE)
-import Node.SimpleRepl (setPrompt, readLine, runRepl, putStrLn)
+import Node.ReadLine (READLINE, Interface, createConsoleInterface, noCompletion, setLineHandler, setPrompt, prompt)
 
-main :: forall e. Eff (console :: CONSOLE, readline :: READLINE | e) Unit
-main = runRepl do
-  setPrompt "mal-user> "
-  loop
-  where
-    loop = do
-      res <- readLine
-      putStrLn $ res
-      putStrLn $ res
-      loop
+rep :: String -> String
+rep s = s
+
+rep_loop :: forall e. Interface -> String -> Eff (console :: CONSOLE, readline :: READLINE | e) Unit
+rep_loop i s = do
+  log $ rep s
+  prompt i
+
+main :: forall e. Eff (console :: CONSOLE, readline :: READLINE, err :: EXCEPTION | e) Unit
+main = do
+  interface <- createConsoleInterface noCompletion
+  setPrompt "mal-user> " 0 interface
+  setLineHandler interface (rep_loop interface)
+  prompt interface
 
